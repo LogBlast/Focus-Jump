962f6e8b98776c9c352745d0a69ffe80b6f1fcbe
LogBlast <esteban.dubois78@gmail.com>
2023-03-01 18:42:51 +0100

    Test Caméra qui suit le joueur

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+ public GameObject player;
+ public float timeOffset;
+ public Vector3 posOffset;
+
+ private Vector3 velocity;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);   //transform correspond à l'objet "Caméra"       
+    }
+}
