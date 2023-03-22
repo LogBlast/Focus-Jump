c3dbca61a0d049ee16d5021c0cd25825b1143870
LogBlast <esteban.dubois78@gmail.com>
2023-03-23 00:13:34 +0100

    test fichier Builds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    // ctrl + b --> Build and Run
+    // ctrl + b --> Build and Run.
 
     public GameObject settingWindow;
 
